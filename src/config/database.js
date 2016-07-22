46daf72f0b5e6f3470f0e3f393264d6aafc8f0b5
Walter Mattingly <waltdakind@gmail.com>
2016-07-22 14:09:19 -0400

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+
+//use CLEARDB_DATABASE_URL, CLEARDB_PASS, CLEARDB_USER
+module.exports = {
+    'connection': {
+        'host': 'us-cdbr-iron-east-04.cleardb.net',
+        'user': 'bafb782cde7b46',
+        'password': 'efe4310c'
+    },
+	'database': 'heroku_ffbd037f93768e1',
+    'users_table': 'users'
+};
+
