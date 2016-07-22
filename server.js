4306df5931b152d5fd14e4151b0490a1e06c7657
Steven Stabile <stevenstabile@gmail.com>
2016-07-22 16:54:08 -0400

    fixed server.js file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var flash    = require('connect-flash');
 // configuration ===============================================================
 // connect to our database
 
-require(__dirname +'./config/passport')(passport); // pass passport for configuration
+require(__dirname +'/config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -41,7 +41,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 
 // routes ======================================================================
-require(./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
