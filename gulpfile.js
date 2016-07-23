ca0648b6dba0656b503f922dc7fcded257327dd7
Walter Mattingly <waltdakind@gmail.com>
2016-07-23 08:07:29 -0400

    heroku deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,46 @@
-import gulp from "gulp";
-import browserify from "browserify";
-import browserSync from "browser-sync";
-import del from "del";
-import babelify from "babelify";
-import sass from "gulp-sass";
-import postcss from "gulp-postcss";
-import autoprefixer from "autoprefixer";
-import source from "vinyl-source-stream";
-import buffer from "vinyl-buffer";
-import gutil from "gulp-util";
-import watchify from "watchify";
-import nodemon from "gulp-nodemon";
+var gulp = require("gulp");
+var browserify = require("browserify");
+var browserSync = require("browser-sync");
+var del = require("del");
+//var babelify = require("babelify");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
+var gutil = require("gulp-util");
+var watchify = require("watchify");
+var nodemon = require("gulp-nodemon");
 
-const b = watchify(browserify({
-    plugin: [watchify],
-    debug: true,
-    cache: {},
-    packageCache: {}
-}));
-b.transform(babelify);
+// const b = watchify(browserify({
+//     plugin: [watchify],
+//     debug: true,
+//     cache: {},
+//     packageCache: {}
+// }));
+// b.transform(babelify);
 
 gulp.task('greet', function () {
     console.log('Gulp started! Hello and good luck with your app :)');
 });
 
-gulp.task("javascript", function() {
-    function rebundle() {
-        const bundle = b.bundle()
-            .pipe(source("app.js"))
-            .pipe(buffer())
-            .on("error", gutil.log)
-            .pipe(gulp.dest("./build/"))
-            .pipe(browserSync.reload({
-                stream: true,
-                once: true
-            }));
+// gulp.task("javascript", function() {
+//     function rebundle() {
+//         const bundle = b.bundle()
+//             .pipe(source("app.js"))
+//             .pipe(buffer())
+//             .on("error", gutil.log)
+//             .pipe(gulp.dest("./build/"))
+//             .pipe(browserSync.reload({
+//                 stream: true,
+//                 once: true
+//             }));
 
-        return bundle;
-    }
+//         return bundle;
+//     }
 
-    b.on("update", rebundle);
-    return rebundle();
+//     b.on("update", rebundle);
+//     return rebundle();
 });
 
 gulp.task("css", function() {
@@ -58,25 +58,30 @@ gulp.task("css", function() {
 
 gulp.task("browser-sync", ["server"], function() {
     browserSync.init({
-        port: 3000,
+        port: process.env.PORT || 8080,
         proxy: "http://localhost:8000"
     });
 });
 
 gulp.task("server", ["build"], function(cb) {
-    nodemon({script: "index.js", env: {"NODE_ENV": "development"}})
+    nodemon({script: "./src/server.js", env: {"NODE_ENV": "production"}})
         .on("start", function() {
             cb();
         });
 });
 
-gulp.task("clean", function(cb) {
-    del(["build"], cb);
-});
+// gulp.task("clean", function(cb) {
+//     del(["build"], cb);
+// });
 
-gulp.task("build", ["javascript", "css"], function(cb) {
+gulp.task("build", ["css"], function(cb) {
     cb();
 });
+
+gulp.task('heroku:production', function(){
+  runSeq('greet', 'clean', 'build', 'server')
+})
+
 
 gulp.task("default", ["greet", "build", "server", "browser-sync"], function() {
     gulp.watch("scss/*.scss", ["css"]);
