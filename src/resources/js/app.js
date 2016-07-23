f339fff3a7b381ec988dbc00a0f973c2cdb2459b
Steven Stabile <stevenstabile@gmail.com>
2016-07-23 10:46:36 -0400

    added js functionality

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	console.log("hello");
 
+	var dt = new Date();
+	var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+	$('.currentTime').append(time);
+	
 
 });
