eda57379d641fc2a5b2fdcf0d40c63971285246f
Steven Stabile <stevenstabile@gmail.com>
2016-07-23 11:55:31 -0400

    added notes and consumers

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,9 +2,24 @@ $(document).ready(function(){
 	console.log("hello");
 
 	var dt = new Date();
-	var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+	var time = dt.getHours() + ":" + dt.getMinutes();
 
 	$('.currentTime').append(time);
 	
+	var consumerArr = ['John Smith', 'Mark Thompson', 'David Ortiz', 'Jerry Mathers', 'Wilson Pickett'];
+	var consumerList = '<ul>';
+
+	for(i=0;i<consumerArr.length; i++){
+		
+		consumerList += '<li>' + consumerArr[i] + '<li>';
+
+	}
+	consumerList += '<ul>';
+
+	// $('#consumerDiv').append(consumerList);
+	$('#addNote').on('click', function(){
+		var newNote = '<li><strong> ' + time + ' </strong>- ' + $('#insertNote').val() + '</li><br>';
+		$('#notesList').prepend(newNote);
+	});
 
 });
